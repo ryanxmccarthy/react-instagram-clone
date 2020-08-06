750a849c8eac6ccee75243c2f9c8f198d40ec79b
Ryan XVX <ryanxmccarthy@gmail.com>
2020-08-06 10:51:27 -0700

    building out server

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import React from "react";
 import "./App.css";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
+//components
 import Header from "./components/Header";
 import Post from "./components/Post";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <section className="App-main">
-        <Post
-          nickname="Chris"
-          avatar="https://picsum.photos/200"
-          caption="Moving the community!"
-          image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg"
-        />
-        <Post
-          nickname="OG"
-          avatar="https://picsum.photos/201"
-          caption="Holding a mic"
-          image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg"
-        />
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Header />
+        <section className="App-main">
+          <Post
+            nickname="ryanxvx"
+            avatar="https://picsum.photos/200"
+            caption="Enjoying the great outdoors!"
+            image={require("./assets/images/IMG_8567.JPG")}
+          />
+          <Post
+            nickname="OG"
+            avatar="https://picsum.photos/201"
+            caption="Holding a mic"
+            image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg"
+          />
 
-        {/* more posts */}
-      </section>
-    </div>
+          {/* more posts */}
+        </section>
+      </div>
+    </ApolloProvider>
   );
 }
 
