41a551a590290d75bbe8b2e415091d68aa273e6b
Ryan XVX <ryanxmccarthy@gmail.com>
2020-08-07 22:40:41 -0700

    rewriting Posts component

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -7,47 +7,57 @@ class Posts extends Component {
   constructor() {
     super();
     this.state = {
-      posts: []
-    }
+      posts: [],
+    };
   }
 
-  return (
-    <Query
-      query={gql`
-        {
-          posts(user_id: "a") {
-            id
-            user {
-              nickname
-              avatar
+  componentDidMount() {
+    this.props.apollo_client
+      .query({
+        query: gql`
+          {
+            posts(user_id: "a") {
+              id
+              user {
+                nickname
+                avatar
+              }
+              image
+              caption
             }
-            image
-            caption
           }
-        }
-      `}
-    >
-      {({ loading, error, data }) => {
-        if (loading) return <p>Loading Posts...</p>;
-        if (error) return <p>Error Fetching Posts...</p>;
-        let posts = data.posts;
+        `,
+      })
+      .then((response) => {
+        this.setState({ posts: response.data.posts });
+      });
 
-        return (
-          <div className="Posts">
-            {posts.map((post) => (
-              <Post
-                nickname={post.user.nickname}
-                avatar={post.user.avatar}
-                image={post.image}
-                caption={post.caption}
-                key={post.id}
-              />
-            ))}
-          </div>
-        );
-      }}
-    </Query>
-  );
-};
+    this.posts_channel = this.props.pusher.subscribe("posts-channel");
+
+    this.posts_channel.bind(
+      "new-post",
+      (data) => {
+        this.setState({ posts: this.state.posts.concat(data.post) });
+      },
+      this
+    );
+  }
+
+  render() {
+    return (
+      <div className="Posts">
+        {this.state.posts.map((post) => (
+          <Post
+            nickname={post.user.nickname}
+            avatar={post.user.avatar}
+            image={post.image}
+            caption={post.caption}
+            key={post.id}
+          />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Posts;
